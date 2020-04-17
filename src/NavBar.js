caf06c0bb41e1cd2666423e0ed55ea9b19eafa90
Dillon Yeh <kyeh5@my.bcit.ca>
2020-04-16 23:59:42 -0700

    Nav bar styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import "./css/navBar.css";
 
 export default class NavBar extends Component {
     render() {
         return (
             <React.Fragment>
                 <nav className="navbar navbar-dark bg-dark mb-3">
-                    <h1 text-align="left">InFOMO</h1>
+                    <div class="headingContainer">
+                        <h1 class="navHeading">UpDATAble</h1>
+                    </div>
                 </nav>
             </React.Fragment>
         )
