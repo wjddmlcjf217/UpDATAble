39de23865db445c804c6c83a64c72ce5b0e0df81
Dillon Yeh <kyeh5@my.bcit.ca>
2020-04-17 02:31:03 -0700

    Added Home commits

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import history from "./history";
+import './css/home.css';
 
 export default class Home extends Component {
     render() {
         return (
             <>
-                <div>
-                    <Button variant="btn btn-success" onClick={() => history.push('/PatientForm')}>Doctor Workflow</Button>
-                </div>
-                <div>
-                    <Button variant="btn btn-success" onClick={() => history.push('/Patient')}>Patient View</Button>
+                <h1 class="homeHeader">Are You a Doctor or a Patient?</h1>
+                <div class="buttonContainer">
+                    <Button className="button1" variant="btn btn-success" onClick={() => history.push('/PatientForm')}>Doctor</Button>
+                    <Button className="button2" variant="btn btn-success" onClick={() => history.push('/Patient')}>Patient</Button>
                 </div>
             </>
         )
